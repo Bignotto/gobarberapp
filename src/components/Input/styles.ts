9c0616e39313d7756c0c57a1d5b30f9c1c4dc814
Thiago Bignotto <bignotto@gmail.com>
2020-07-29 23:39:35 -0300

    input and button components done

---
+++ src/components/Input/styles.ts
@@ -0,0 +1,26 @@
+import styled from "styled-components/native";
+
+import Fi from "react-native-vector-icons/Feather";
+
+export const Container = styled.View`
+  width: 100%;
+  height: 60px;
+  padding: 0 16px;
+  border-radius: 10px;
+  margin-bottom: 8px;
+  background: #232129;
+
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const TextInput = styled.TextInput`
+  flex: 1;
+  color: #fff;
+  font-size: 16px;
+  font-family: "RobotoSlab-Regular";
+`;
+
+export const Icon = styled(Fi)`
+  margin-right: 16px;
+`;
--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -3,12 +3,18 @@ import React from "react";
 import { Image } from "react-native";
 import { Container, Title } from "./styles";
 
+import Button from "../../components/Button";
+import Input from "../../components/Input";
+
 import logoImg from "../../assets/logo.png";
 
 const SignIn: React.FC = () => (
   <Container>
     <Image source={logoImg} />
     <Title>Faça seu logon:</Title>
+    <Input name="email" icon="mail" placeholder="E-Mail" />
+    <Input name="password" icon="lock" placeholder="Password" />
+    <Button onPress={() => {}}>Entrar</Button>
   </Container>
 );
 
